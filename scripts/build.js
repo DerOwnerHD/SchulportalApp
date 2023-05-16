87fa13995ba7a647611cb937ec8c55b3f2c9d26f
Tim <timthiede1@gmail.com>
2023-05-16 17:32:10 +0200

    Changes -> frontend now doesn't get minified on Windows -> fixed font loading

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,7 @@
 const { minify } = require("terser");
 const htmlMinifier = require("html-minifier");
 const fs = require("fs");
+const os = require("os");
 
 /**
  * Minifies all Frontend scripts, styles and
@@ -10,15 +11,27 @@ const fs = require("fs");
 async function buildFrontend() {
     const scripts = fs.readdirSync("./frontend/js");
     console.log("[Minifier] Loaded " + scripts.length + " scripts");
-    let minifiedScript = "";
+
+    let scriptContent = "";
+    if (os.platform() === "win32") {
+
+        console.log("[Minifier] Running on Windows, thus not minifying scripts");
+        for (const script of scripts)
+            scriptContent += fs.readFileSync("./frontend/js/" + script);
+        fs.writeFileSync("./build/frontend/app.js", scriptContent);
+        fs.writeFileSync("./build/frontend/index.html", fs.readFileSync("./frontend/index.html").toString());
+        return;
+
+    }
+
     for (const script of scripts)
         await minify(fs.readFileSync("./frontend/js/" + script).toString())
             .then((minified) => { 
                 console.log("[Minifier] Minified script " + script);
-                minifiedScript += "\n\n" + minified.code;
+                scriptContent += "\n" + minified.code;
             }).catch(() => { console.log("[Minifier] Couldn't minify script " + script); });
 
-    fs.writeFileSync("./build/frontend/app.js", minifiedScript);
+    fs.writeFileSync("./build/frontend/app.js", scriptContent);
     console.log("[Minifier] Sucessfully minified scripts");
 
     const html = htmlMinifier.minify(fs.readFileSync("./frontend/index.html").toString(), {
